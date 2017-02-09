244f968bd2be5d6405331043fb3be21ca38fee25
michael.fan <mchl_fan@yahoo.com>
2017-02-08 23:01:34 -0500

    Practice this.props

--- app/index.js
+++ app/index.js
@@ -1,15 +1,53 @@
+//cd Desktop/"Code projects"/"Beginner react"/github-battle-take-two
+
+var USER_DATA = {
+  name: 'Gordon Ramsay',
+  username: 'Gordon_Ramsay',
+  image: 'http://www.myfoxspokane.com/wp-content/uploads/2016/05/CroppedFocusedImage160055050-50-gordon-ramsay-2.jpg'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
+  render: function() {
+    return <img src={this.props.imageUrl} />
+  }
+});
+
+var ProfileName = React.createClass({
   render: function() {
     return (
-      <div> Hello Michael! </div>
+      <div>{this.props.name}</div>
     )
   }
 });
 
+var ProfileLink = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <a href={'https://en.wikipedia.org/wiki/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+})
+
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
